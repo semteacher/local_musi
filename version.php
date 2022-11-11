60b79060eab6b23ac5e824a8c4b24d74808500c6
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2022-11-11 10:49:46 +0100

    Version 0.3.5 (2022111100)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_musi';
-$plugin->release = '0.3.4';
-$plugin->version = 2022110900;
+$plugin->release = '0.3.5';
+$plugin->version = 2022111100;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
