12007297279022385243501d31ea24b46ee4fa4e
georgmaisser <georg.maisser@wunderbyte.at>
2022-12-15 22:19:56 +0100

    Version 0.4.0 (2022121500)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_musi';
-$plugin->release = '0.3.9';
-$plugin->version = 2022120200;
+$plugin->release = '0.4.0';
+$plugin->version = 2022121500;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
